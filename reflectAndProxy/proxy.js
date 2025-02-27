eac14bcd37427eb32d8ef14dcd557267976a0a44
rakshith_s <rakshithsantosh@gmail.com>
2025-02-27 19:55:08 +0530

    Setting my proxy for an object

---
+++ reflectAndProxy/proxy.js
@@ -0,0 +1,31 @@
+const p1 = {
+  fname: "firstname",
+  lname: "lastname",
+  age: 22,
+};
+
+//proxy takes in object reference and a handler function as inputs
+const p1Proxy = new Proxy(p1, {
+  get(target, prop) {
+    if (prop in target) return target[prop];
+    return false;
+  },
+  set(target, prop, value) {
+    if (!(prop in target)) throw new Error(`${prop} does not exists`);
+    switch (prop) {
+      case "firstname":
+      case "lastname":
+        if (typeof value !== "string")
+          throw new Error(`${prop} must be a string `);
+        break;
+      case "age":
+        if (typeof value !== "number")
+          throw new Error(`${prop} must be a number `);
+        if (value <= 0) throw new Error(`${prop} must be greater than zero`);
+    }
+    target[prop] = value;
+  },
+});
+
+p1Proxy.age = 1;
+console.log(p1Proxy.age);
