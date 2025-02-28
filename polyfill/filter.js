21e0281ee31ac96ed7346e88818a5ea50f3743e7
rakshith_s <rakshithsantosh@gmail.com>
2025-02-28 20:02:48 +0530

    polyfill for filter

---
+++ polyfill/filter.js
@@ -0,0 +1,16 @@
+Array.prototype.filter = function (cb, thisArg) {
+  if (typeof cb !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
+  if (!Array.isArray(this)) {
+    throw new TypeError("Called on a non-array object");
+  }
+  let result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (cb.call(thisArg, this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
